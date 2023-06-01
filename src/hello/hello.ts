53e42026f1c20bb4e7370739530b788b25f78b35
Jason Merrill <jwmerrill@gmail.com>
2023-06-01 14:31:41 -0400

    Initial structure

---
+++ src/hello/hello.ts
@@ -0,0 +1,5 @@
+import { LogFn, message } from 'main/const'
+
+export const hello: LogFn = () => {
+  console.log(message);
+}
---
+++ src/main/const.ts
@@ -0,0 +1,3 @@
+export const message = "Hello World";
+
+export type LogFn = ()=>void
